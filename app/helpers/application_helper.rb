0d543fb832ed0c476193d213c6c4f0e64168e62e
Alexis Cerna <afcerna@uc.cl>
2017-04-25 11:55:04 -0300

    test push

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Return the full title on a per-page basis
   def full_title(page_title = '')
-    base_title = "Ruffle-Mania"
+    base_title = "Raffle-Mania"
     if page_title.empty?
       "#{base_title}"
     else
