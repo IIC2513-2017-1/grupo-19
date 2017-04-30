1104961ce9d57ef0c1945e3408391bf70715657b
Oscar Avila <onavila@uc.cl>
2017-04-30 19:15:00 -0300

    Finish model for Messages_Responses

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,8 @@ class Message < ApplicationRecord
   belongs_to :sender, foreign_key: 'sender_id', class_name: "User";
   belongs_to :recipient, foreign_key: 'recipient_id', class_name: "User"
 
-  #has_many :message_responses
+  has_many :message_responses, foreign_key: 'message_responded_id',
+                               class_name: 'MessageResponse'
+  has_one :message_responded, foreign_key: 'message_responses_id',
+                               class_name: 'MessageResponse'
 end
--- app/models/message_response.rb
+++ app/models/message_response.rb
@@ -1,4 +1,6 @@
 class MessageResponse < ApplicationRecord
-  #belongs_to :message
-  #belongs_to :message_responded, class_name: "Message"
+  belongs_to :message_response, foreign_key: 'message_response_id',
+                                class_name: 'Message'
+  belongs_to :message_responded, foreign_key: 'message_responded_id',
+                                 class_name: 'Message'
 end
