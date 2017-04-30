997700402a705f2942ed938bea4c6d9dedc8283f
Oscar Avila <onavila@uc.cl>
2017-04-30 05:09:06 -0300

    Finish user edit, update, index, and destroy actions. Tests pending

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: "Usuario Administrador",
+             email: "usuario@administrador.cl",
+             password: "foobar",
+             password_confirmation: "foobar",
+             admin: true)
+
+20.times do |n|
+  name = Faker::Name.name
+  email = Faker::Internet.unique.email
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
