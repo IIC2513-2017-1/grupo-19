f8473413cbcbc6c00f2e2ab6509d536c4adbaaaf
Alexis Cerna <afcerna@uc.cl>
2017-04-30 21:56:39 -0300

    readme rutas

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 To see the application in the production environment go to:
 https://salty-garden-46479.herokuapp.com
+
+rutas:
+'/users'
+'/raffles'
+'/numbers'
+'/prices'
+'/notifications'
+'/messages'
+'/winners'
+'/purchases'
--- db/schema.rb
+++ db/schema.rb
@@ -40,12 +40,12 @@ ActiveRecord::Schema.define(version: 20170430233347) do
   end
 
   create_table "notifications", force: :cascade do |t|
-    t.integer  "raffle_id"
-    t.integer  "user_id"
     t.text     "content"
-    t.boolean  "seen"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "raffle_id"
+    t.integer  "user_id"
+    t.boolean  "seen"
   end
 
   create_table "numbers", force: :cascade do |t|
