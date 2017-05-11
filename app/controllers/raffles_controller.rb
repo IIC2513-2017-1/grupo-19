d11a59a1ede2a14624a12ea8a55de17d5c97dfff
Oscar Avila <onavila@uc.cl>
2017-05-11 16:56:08 -0300

    Finish validations for raffles

--- app/controllers/raffles_controller.rb
+++ app/controllers/raffles_controller.rb
@@ -1,6 +1,8 @@
 class RafflesController < ApplicationController
   before_action :set_raffle, only: [:show, :edit, :update, :destroy]
-  before_action :logged_in_user, only: [:edit, :new, :create, :update, :destroy]
+  before_action :logged_in_user, only: [:new, :create, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :admin_user, only: [:destroy]
 
   # GET /raffles
   # GET /raffles.json
