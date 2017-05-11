9450f5614234ea78f2138cbc39336efb7daa4fbe
Oscar Avila <onavila@uc.cl>
2017-05-11 13:32:29 -0300

    User cannot create a new user while logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :not_logged_in_user, only: :new
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
 
@@ -73,5 +74,12 @@ class UsersController < ApplicationController
     # Confirms an admin user
     def admin_user
       redirect_to(root_url) unless current_user.admin?
+    end
+
+    # Confirms a not logged-in user
+    def not_logged_in_user
+      unless not logged_in?
+        redirect_to current_user
+      end
     end
 end
