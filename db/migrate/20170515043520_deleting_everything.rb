d24105a4ce044eea5d12d14d1bb9184dd2daeb3c
Oscar Avila <onavila@uc.cl>
2017-06-26 23:40:47 -0400

    Deleted obstructing migration

--- db/migrate/20170515043520_deleting_everything.rb
+++
@@ -1,7 +0,0 @@
-class DeletingEverything < ActiveRecord::Migration[5.0]
-  def up
-      remove_columns :messages, :subject, :sender_id, :recipient_id
-      drop_table :message_responses
-  end
-
-end
