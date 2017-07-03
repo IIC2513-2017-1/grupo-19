f90177a6f9d4f2b5266c93984614bbfc8d149fae
Oscar Avila <onavila@uc.cl>
2017-07-03 15:57:52 -0400

    Fix paperclip in development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Comunicate to Paperclip the path to the image procesor
+  Paperclip.options[:command_path] = "/usr/bin/convert"
 end
